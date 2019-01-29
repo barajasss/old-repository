8e617569fe9e2a7c26b6c3eb135c86d9e4aa4248
Baraja Swargiary <43605760+barajasss@users.noreply.github.com>
2019-01-29 22:00:45 +0530

    Add files via upload

--- baraja portfolio/scripts/scroll.js
+++ baraja portfolio/scripts/scroll.js
@@ -1,22 +1,29 @@
 
 //javascript file to smoothly scroll
 
-var homeBtn = document.getElementById("homeBtn");
-var skillsBtn = document.getElementById("skillsBtn");
-var projectsBtn = document.getElementById("projectsBtn");
-var contactBtn = document.getElementById("contactBtn");
+function _(element){
+	return document.getElementById(element);
+}
+
+var homeBtn = _("homeBtn");
+var skillsBtn = _("skillsBtn");
+var projectsBtn = _("projectsBtn");
+var contactBtn = _("contactBtn");
 var buttons = document.getElementsByClassName("nav-btn");
 var buttons = Array.from(buttons);
+var viewMoreBtn = _("view-more-btn");
+var topBtn = _("top-btn");
 
-var homeElement = document.getElementById("home");
-var skillsElement = document.getElementById("skills");
-var projectsElement = document.getElementById("projects");
-var contactElement = document.getElementById("contact");
+var homeElement = _("home");
+var skillsElement = _("skills");
+var projectsElement = _("projects");
+var contactElement = _("contact");
 
 homeBtn.addEventListener("click", moveToHome, false);
 skillsBtn.addEventListener("click", moveToSkills, false);
 projectsBtn.addEventListener("click", moveToProjects, false);
 contactBtn.addEventListener("click", moveToContact, false);
+viewMoreBtn.addEventListener("click", moveToSkills, false);
 
 var scrollAmount = 20;
 var scrollSpeed = 8;
@@ -42,7 +49,7 @@ function moveToSkills(e){
 function moveToProjects(e){
 	mousedown = false;
 	updateBtn(projectsBtn);
-	targetY = projectsElement.offsetTop-40;
+	targetY = projectsElement.offsetTop-50;
 	interval = setInterval(function(){
 		scrollTo(targetY);
 	}, scrollSpeed);
@@ -58,6 +65,7 @@ function moveToContact(e){
 
 
 function updateBtn(elem){
+	clearInterval(interval);
 	for(var i in buttons){
 		buttons[i].setAttribute("class", "nav-btn");
 	};
@@ -93,23 +101,39 @@ document.addEventListener("mousedown", function(){
 document.addEventListener("touchmove", function(){
 	mousedown = true;
 }, false);
+
+//scroll event to update the button styles
+
+topBtn.onmouseover = function(){
+	topBtn.style.opacity = 1;
+	topBtn.style.cursor = "pointer";
+}
 window.addEventListener("scroll", function(){
+
+	//control the visibility of top button
+	if(window.scrollY > 500){
+		topBtn.style.opacity = 0.8;
+		topBtn.style.visibility = "visible";
+	}
+	else{
+		topBtn.style.opacity = 0;
+		topBtn.style.visibility = "hidden";
+	}
+
+	//control styles of navigation buttons
 	if(mousedown){
+		clearInterval(interval);
 		var y = window.scrollY;
 		if(y > contactElement.offsetTop-350){
-			console.log("scroll contact btn");
 			updateBtn(contactBtn);
 		}
-		else if(y > projectsElement.offsetTop-50){
-			console.log("scroll projects btn");
+		else if(y > projectsElement.offsetTop-5){
 			updateBtn(projectsBtn);
 		}
-		else if(y > skillsElement.offsetTop-40){
-			console.log("scroll skills btn");
+		else if(y > skillsElement.offsetTop-4){
 			updateBtn(skillsBtn);
 		}
-		else if(y > homeElement.offsetTop-10){
-			console.log("scroll home btn");
+		else if(y > homeElement.offsetTop-1){
 			updateBtn(homeBtn);
 		}
 	}
@@ -118,4 +142,25 @@ window.addEventListener("scroll", function(){
 
 
 
+viewMoreBtn.addEventListener("click", function(e){
+	e.preventDefault();
+}, false);
 
+
+topBtn.addEventListener("click", function(){
+	var i=0;
+	//clear the previous interval if any
+	interval2 = setInterval(function(){
+		var y = window.scrollY;
+		i++;
+		if(y > 10){
+			console.log("top btn runngin", i);
+			y -= scrollAmount;
+			window.scroll(0, y);
+		}
+		else{
+			console.log("cleared");
+			clearInterval(interval2);
+		}
+	}, 10);
+}, false);
